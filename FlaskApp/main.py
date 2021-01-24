2a020120449e38fbf6331c8ee21b2d0888d9447a
Evan Putnam <emput1998@gmail.com>
2021-01-23 22:30:29 -0500

    Re-arranging some things

--- FlaskApp/main.py
+++ FlaskApp/main.py
@@ -14,31 +14,6 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 5 * 1000 * 1000 # Specifies rough max size of upload in mb.
 
-def verify_file(file: str):
-    if file != "" and "." in file and file.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS:
-        filename = secure_filename(file)
-        return filename
-    return None
-
-def process_image(verified_file, file):
-    # Download file from user.
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], verified_file)
-    if os.path.exists(file_path): os.remove(file_path)
-    file.save(file_path)
-
-    # Process file.
-    out_path = os.path.join(PROCESSED_FILES_FOLDER, verified_file)
-    if os.path.exists(out_path): os.remove(out_path)
-    os.system(PATH_OF_KINKADE_TRANSFER_LEARNING_APP + " \"" + file_path + "\"" + " \"" + out_path + "\"")
-    
-    if os.path.exists(out_path):
-        return out_path
-    else:
-        return ""
-
-@app.route('/kinkade_file', methods = ['GET'])
-def kinkade_file():
-    return "SUCCESS!"
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
@@ -66,9 +41,29 @@ def index():
         if "invalid_upload" in request.args:
             invalid_upload = True
         return render_template('index.html', invalid_upload = invalid_upload)
+
+
+def verify_file(file: str):
+    if file != "" and "." in file and file.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS:
+        filename = secure_filename(file)
+        return filename
+    return None
+
+def process_image(verified_file, file):
+    # Download file from user.
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], verified_file)
+    if os.path.exists(file_path): os.remove(file_path)
+    file.save(file_path)
+
+    # Process file.
+    out_path = os.path.join(PROCESSED_FILES_FOLDER, verified_file)
+    if os.path.exists(out_path): os.remove(out_path)
+    os.system(PATH_OF_KINKADE_TRANSFER_LEARNING_APP + " \"" + file_path + "\"" + " \"" + out_path + "\"")
     
+    if os.path.exists(out_path):
+        return out_path
+    else:
+        return ""
 
 if __name__ == "__main__":
     app.run()
-        
-
